4648d918f00370ba758f03daf2280c8f5100240c
thinh <phamhoangthinhc9@gmail.com>
2024-09-18 20:10:28 +0700

    Add UI Package

---
+++ Assets/BaseGame/Scripts/Core/UI/Launcher.cs
@@ -0,0 +1,23 @@
+using Cysharp.Threading.Tasks;
+using TW.UGUI.Core.Modals;
+using TW.UGUI.Core.Screens;
+using TW.UGUI.Core.Views;
+using TW.UGUI.Core.Windows;
+
+using ZBase.UnityScreenNavigator.Core;
+
+public class Launcher : UnityScreenNavigatorLauncher
+{
+    protected override void Start()
+    {
+        base.Start();
+        // ShowLoadingScreen().Forget();
+    }
+
+    // private async UniTaskVoid ShowLoadingScreen()
+    // {
+    //     //ViewOptions options = new ViewOptions(nameof(ScreenTitle), false, loadAsync: false);
+    //     ViewOptions options = new ViewOptions(nameof(ScreenMainMenu));
+    //     await ScreenContainer.Find(ContainerKey.Screens).PushAsync(options);
+    // }
+}
