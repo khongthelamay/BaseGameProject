5f137a875fef90c12f26a18660ddf5e3e26bf094
hoa chu <hoacmgch18105@gmail.com>
2024-09-21 19:25:18 +0700

    update UI

---
+++ Assets/BaseGame/Scripts/Core/UI/Modal/ModalArtifactInforContext.cs
@@ -0,0 +1,101 @@
+using System;
+using Cysharp.Threading.Tasks;
+using TW.UGUI.MVPPattern;
+using UnityEngine;
+using R3;
+using Sirenix.OdinInspector;
+using TW.Reactive.CustomComponent;
+using UGUI.Core.Modals;
+using TMPro;
+using UnityEngine.UI;
+using TW.UGUI.Core.Modals;
+
+[Serializable]
+public class ModalArtifactInforContext 
+{
+    public static class Events
+    {
+        public static Subject<Unit> SampleEvent { get; set; } = new();
+    }
+    
+    [HideLabel]
+    [Serializable]
+    public class UIModel : IAModel
+    {
+        [field: Title(nameof(UIModel))]
+        [field: SerializeField] public ReactiveValue<int> SampleValue { get; private set; }
+        public ReactiveValue<ArtifactDataConfig> currentArtifactDataConfig;
+        public UniTask Initialize(Memory<object> args)
+        {
+            currentArtifactDataConfig = ArtifactManager.Instance.currentArtifactChoose;
+            
+            return UniTask.CompletedTask;
+        }
+    }
+    
+    [HideLabel]
+    [Serializable]
+    public class UIView : IAView
+    {
+        [field: Title(nameof(UIView))]
+        [field: SerializeField] public CanvasGroup MainView {get; private set;}  
+        [field: SerializeField] public TextMeshProUGUI txtName {get; private set;}  
+        [field: SerializeField] public TextMeshProUGUI txtLevel {get; private set;}  
+        [field: SerializeField] public TextMeshProUGUI txtFunDes {get; private set;}  
+        [field: SerializeField] public TextMeshProUGUI txtDes {get; private set;}  
+        [field: SerializeField] public TextMeshProUGUI txtUpgradeRequire {get; private set;}  
+        [field: SerializeField] public ProgressBar piecesProgress {get; private set;}  
+        [field: SerializeField] public Button btnExit {get; private set;}  
+        [field: SerializeField] public Button btnUpgrade {get; private set;}  
+        
+        public UniTask Initialize(Memory<object> args)
+        {
+            return UniTask.CompletedTask;
+        }
+
+        public void InitData(ArtifactDataConfig artifactDataConfig) {
+            txtName.text = artifactDataConfig.strName;
+            txtDes.text = artifactDataConfig.strDes;
+            txtFunDes.text = artifactDataConfig.strFunDes;
+            //txtLevel
+            txtUpgradeRequire.text = artifactDataConfig.priceUpgrade[0].ToStringUI();
+        }
+    }
+
+    [HideLabel]
+    [Serializable]
+    public class UIPresenter : IAPresenter, IModalLifecycleEventSimple
+    {
+        [field: SerializeField] public UIModel Model {get; private set;} = new();
+        [field: SerializeField] public UIView View { get; set; } = new();        
+        [Button]
+        public async UniTask Initialize(Memory<object> args)
+        {
+            Debug.Log("Init");
+            await Model.Initialize(args);
+            await View.Initialize(args);
+
+            Model.currentArtifactDataConfig.ReactiveProperty.Subscribe(ChangeData);
+
+            View.btnExit.onClick.AddListener(OnCloseModal);
+            View.btnUpgrade.onClick.AddListener(UpgradeArtifact);
+        }
+
+        void UpgradeArtifact() { }
+
+        void OnCloseModal() {
+            Debug.Log("Close it");
+            ModalContainer.Find(ContainerKey.Modals).Pop(true);
+        }
+
+        private void ChangeData(ArtifactDataConfig artifactDataConfig)
+        {
+            View.InitData(artifactDataConfig);
+        }
+
+        public UniTask Cleanup(Memory<object> args) {
+            Events.SampleEvent = null;
+            return UniTask.CompletedTask;
+        }
+    }
+}
---
+++ Assets/BaseGame/Scripts/GlobalConfig/ArtifactGlobalConfig.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Sirenix.Utilities;
+using TW.Utility.CustomType;
+using System.Collections.Generic;
+using System;
+
+[CreateAssetMenu(fileName = "ArtifactGlobalConfig", menuName = "GlobalConfigs/ArtifactGlobalConfig")]
+[GlobalConfig("Assets/Resources/GlobalConfig/")]
+public class ArtifactGlobalConfig : GlobalConfig<ArtifactGlobalConfig>
+{
+    public List<ArtifactDataConfig> artifactDataConfigs = new();
+
+    public ArtifactDataConfig GetArtifactDataConfig(ArtifactType artifactType)
+    {
+        return artifactDataConfigs.Find(e => e.artifactType == artifactType);
+    }
+}
+
+public enum ArtifactType { 
+    None = 0,
+    PowerPotion = 1,
+    FairyBow = 2,
+    PunchingGlove = 3,
+    Greatsword = 4,
+    ArcaneTome = 5,
+    Snail = 6,
+    Bat = 7,
+    WizardsHat = 8,
+    Wallet = 9,
+    CastleWall = 10
+}
+
+[System.Serializable]
+public class ArtifactDataConfig {
+    public ArtifactType artifactType;
+    public string strName;
+    public string strDes;
+    public string strFunDes;
+    public List<BigNumber> priceUpgrade = new();
+    public List<int> piecesRequire = new();
+}
