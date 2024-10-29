de61ccbfc5b79a08ba7a02d38aec41408cd47bfe
hoa chu <hoacmgch18105@gmail.com>
2024-10-29 20:22:07 +0700

    update screen

--- Assets/BaseGame/Scripts/Core/UI/Screen/ScreensRecruitContext.cs
+++ Assets/BaseGame/Scripts/Core/UI/Screen/ScreensRecruitContext.cs
@@ -6,6 +6,8 @@ using R3;
 using Sirenix.OdinInspector;
 using TW.Reactive.CustomComponent;
 using TW.UGUI.Core.Screens;
+using UnityEngine.UI;
+using TW.UGUI.Core.Views;
 
 [Serializable]
 public class ScreensRecruitContext 
@@ -34,6 +36,9 @@ public class ScreensRecruitContext
     {
         [field: Title(nameof(UIView))]
         [field: SerializeField] public CanvasGroup MainView {get; private set;}  
+        [field: SerializeField] public Button btnExit {get; private set;}
+        [field: SerializeField] public ButtonNotice btnRecruit { get; private set; }
+        [field: SerializeField] public ButtonNotice btnRecruitX10 {get; private set;}  
         
         public UniTask Initialize(Memory<object> args)
         {
@@ -52,6 +57,25 @@ public class ScreensRecruitContext
         {
             await Model.Initialize(args);
             await View.Initialize(args);
-        }      
+
+            View.btnExit.onClick.AddListener(OnClose);
+            View.btnRecruit.SetButtonOnClick(Recruit);
+            View.btnRecruitX10.SetButtonOnClick(RecruitX10);
+        }
+
+        void OnClose() {
+
+            ScreenContainer.Find(ContainerKey.Screens).PopAsync(true);
+
+            ViewOptions options = new ViewOptions(nameof(ScreensDefault));
+            ScreenContainer.Find(ContainerKey.MidleScreens).PushAsync(options);
+
+            options = new ViewOptions(nameof(ScreensBattle));
+            ScreenContainer.Find(ContainerKey.Screens).PushAsync(options);
+        }
+
+        void Recruit() { }
+
+        void RecruitX10() { }
     }
 }
