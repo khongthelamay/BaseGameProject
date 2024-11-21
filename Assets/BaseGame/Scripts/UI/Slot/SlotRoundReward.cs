daa58f9468c0f3e81311306b4635fccdc7d0a6c3
hoa chu <hoacmgch18105@gmail.com>
2024-11-21 22:44:34 +0700

    u

--- Assets/BaseGame/Scripts/UI/Slot/SlotRoundReward.cs
+++ Assets/BaseGame/Scripts/UI/Slot/SlotRoundReward.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SlotRoundReward : SlotBase<RoundRewardConfig>
 {
-    
+    [Header("---- Slot Round Reward ----")]
+    public TextMeshProUGUI txtLevel;
+    public override void InitData(RoundRewardConfig data)
+    {
+        base.InitData(data);
+        txtLevel.text = data.level.ToString();
+    }
 }
