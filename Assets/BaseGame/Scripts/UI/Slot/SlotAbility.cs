5ec68f48f94e6f09106e68d49fa7bf68d0ced90f
hoa chu <hoacmgch18105@gmail.com>
2024-11-21 22:07:15 +0700

    remove ability

--- Assets/BaseGame/Scripts/UI/Slot/SlotAbility.cs
+++ Assets/BaseGame/Scripts/UI/Slot/SlotAbility.cs
@@ -1,12 +1,12 @@
 using Core;
 using UnityEngine;
 
-public class SlotAbility : SlotBase<Ability>
+public class SlotAbility : MonoBehaviour// SlotBase<Ability>
 {
-    [Header("==== Ability Slot ====")]
-    [SerializeField] GameObject objLock;
-    public override void InitData(Ability data)
-    {
-        base.InitData(data);
-    }
+    //[Header("==== Ability Slot ====")]
+    //[SerializeField] GameObject objLock;
+    //public override void InitData(Ability data)
+    //{
+    //    base.InitData(data);
+    //}
 }
